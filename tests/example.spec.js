39d2c154f66916d984a5fbaa2c4c4707a13738e5
Jovanni Feliz <feliz.jovanni@gmail.com>
2025-03-15 22:04:47 -0500

    fixed link typo

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
 
-  await page.goto('http://127.0.0.1:5500/index.html');
+  await page.goto('https://brandtestimony.netlify.app/');
 
   // Expect a title "to contain" a substring.
   await expect(page.getByText('Sarah Dole')).toBeVisible();;
